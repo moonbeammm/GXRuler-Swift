1d1bedb8171d2a54497f986b9157336bd2a61cb2
sunguangxin <sunguangxin@bilibili.com>
2018-03-04 23:19:49 +0800

    添加测试类

---
+++ GXRuler-Swift/GXRulerMain.swift
@@ -0,0 +1,17 @@
+//
+//  GXRulerMain.swift
+//  GXRuler-Swift
+//
+//  Created by sunguangxin on 2018/3/4.
+//  Copyright © 2018年 sunguangxin. All rights reserved.
+//
+
+import Foundation
+
+public class GXRulerMain: NSObject {
+
+    public func RulerTestMethod (_ animation : Bool) {
+        print("我是GXRuler-Swift库里的 RulerTestMethod!!\n你成功调用了自定义framework里的方法!!")
+    
+    }
+}
---
+++ GXRuler-Swift/GXRuler_Swift.h
@@ -0,0 +1,19 @@
+//
+//  GXRuler_Swift.h
+//  GXRuler-Swift
+//
+//  Created by sunguangxin on 2018/3/4.
+//  Copyright © 2018年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GXRuler_Swift.
+FOUNDATION_EXPORT double GXRuler_SwiftVersionNumber;
+
+//! Project version string for GXRuler_Swift.
+FOUNDATION_EXPORT const unsigned char GXRuler_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GXRuler_Swift/PublicHeader.h>
+
+
